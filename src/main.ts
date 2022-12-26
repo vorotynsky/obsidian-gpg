aad9ee4ad0bf2b233196af22674fce7500c972cf
Maxim Vorotynsky <vorotynsky.maxim@gmail.com>
2022-12-26 03:14:47 +0300

    feat: postprocessor for gpg block code

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { Plugin } from 'obsidian';
 export default class ObsidianGpg extends Plugin {
 	async onload() {
 		await this.loadSettings();
+
+		this.registerMarkdownCodeBlockProcessor("gpg", (src, el, context) => {
+			const pre = el.createEl('pre')
+			pre.addClass("secret-gpg")
+
+			const code = pre.createEl('code')
+			code.textContent = "🔐 SECRET GPG"
+		})
 	}
 
 	onunload() {
